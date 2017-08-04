a5d1bf5a3e99ff2ce5ac020c0eb3c9ea5a3fab93
Serhiy <s.prihodko@bytesarea.com>
2017-08-04 18:12:47 +0300

    Use localStorage to store data (primary version, it is not too smart enough for now)

--- client/app/components/shared/products.service.js
+++ client/app/components/shared/products.service.js
@@ -1,43 +1,26 @@
 class ProductsListService {
-  constructor() {
-    this.products = [
-      {
-        id: 0,
-        title: 'Book1',
-        price: 120.35,
-        description: 'Great book!'
-      },
-      {
-        id: 1,
-        title: 'Book2',
-        price: 93.5,
-        description: 'Norm'
-      },
-      {
-        id: 2,
-        title: 'Book3',
-        price: 345.1,
-        description: 'Expensive ('
-      },
-      {
-        id: 3,
-        title: 'Book4',
-        price: 40.0,
-        description: 'Cheap'
-      },
-      {
-        id: 4,
-        title: 'Book5',
-        price: 149.99,
-        description: 'Nope'
-      }
-    ];
+  constructor(localStorageService) {
+    'ngInject';
+    this.localStorageService = localStorageService;
+
+    if (!this.localStorageService.isSupported) {
+      console.warn('Local storage is not supported');
+    }
+
+    this.products = this.fetchProducts() || [];
+  }
+
+  fetchProducts () {
+    return this.localStorageService
+      .get('items');
   }
 
   createProduct (product) {
     const id = this.products.length + 1;
     const newProduct = Object.assign({}, product, { id });
     this.products.push(newProduct);
+
+    this.localStorageService.set('items', this.products);
   }
 
   getProducts () {
--- client/app/components/shared/shared.js
+++ client/app/components/shared/shared.js
@@ -1,10 +1,19 @@
 import angular from 'angular';
+import localStorageModule from 'angular-local-storage';
+
 import productsService from './products.service';
 
-let sharedModule = angular.module('about', [])
 
-  .service('ProductsService', productsService)
+let sharedModule = angular.module('about', [
+  localStorageModule
+])
+.service('ProductsService', productsService)
+.config((localStorageServiceProvider) => {
+  'ngInject';
 
-  .name;
+  localStorageServiceProvider
+    .setPrefix('productsStore');
+})
+.name;
 
 export default sharedModule;
